b4112eb0edfa047f6e3ecc8ed04e6a79be0307b3
tahapek5454 <tahapek5454@gmail.com>
2024-02-04 18:10:31 +0300

    feat: make nullable some property

--- MicroServices/Cinema.Services.MovieAPI/Models/Entities/Movie.cs
+++ MicroServices/Cinema.Services.MovieAPI/Models/Entities/Movie.cs
@@ -5,9 +5,9 @@ namespace Cinema.Services.MovieAPI.Models.Entities
     public class Movie : BaseEntity
     {
         public string Name { get; set; }
-        public string Name_EN { get; set; }
+        public string? Name_EN { get; set; }
         public string Description { get; set; }
-        public string Description_EN { get; set; }
+        public string? Description_EN { get; set; }
         public double Point { get; set; }
         public double Time { get; set; }
         public int CategoryId { get; set; }
