76795c31ace7a3346401bae7ff1c4e7225521f1a
tahapek5454 <tahapek5454@gmail.com>
2024-02-04 23:38:04 +0300

    feat: add CategoriesController

---
+++ MicroServices/Cinema.Services.CategoryAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,90 @@
+﻿using Cinema.Services.CategoryAPI.Mapper;
+using Cinema.Services.CategoryAPI.Models.Dtos.Categories;
+using Cinema.Services.CategoryAPI.Models.Entities;
+using Cinema.Services.CategoryAPI.Services.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SharedLibrary.Models.Dtos;
+
+namespace Cinema.Services.CategoryAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoriesController(ICategoryService _categoryService) : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMovieById([FromRoute] int id)
+        {
+            var category = await _categoryService.Table.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (category is null)
+                throw new Exception("Category is not found");
+
+            var categoryDto = ObjectMapper.Mapper.Map<CategoryDto>(category);
+
+            return Ok(ResponseDto<CategoryDto>.Sucess(categoryDto, 200));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMoviesWithPagination([FromQuery] PaginationDto paginationDto)
+        {
+            var categories = await _categoryService.Table
+                .Skip((paginationDto.Page - 1) * paginationDto.Size)
+                .Take(paginationDto.Size)
+                .ToListAsync();
+
+            var categoryDtos = ObjectMapper.Mapper.Map<List<CategoryDto>>(categories);
+
+            return Ok(ResponseDto<List<CategoryDto>>.Sucess(categoryDtos, 200));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllMovies()
+        {
+            var categories = await _categoryService.Table.ToListAsync();
+
+            var categoryDtos = ObjectMapper.Mapper.Map<List<CategoryDto>>(categories);
+
+            return Ok(ResponseDto<List<CategoryDto>>.Sucess(categoryDtos, 200));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMovie([FromBody] AddCategoryDto addCategoryDto)
+        {
+            var newCategory = ObjectMapper.Mapper.Map<Category>(addCategoryDto);
+
+            await _categoryService.Table.AddAsync(newCategory);
+
+            await _categoryService.SaveChangesAsync();
+
+            return Created($"GetMovieById/{newCategory.Id}", ResponseDto<BlankDto>.Sucess(200));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateMovie([FromBody] UpdateCategoryDto updateCategoryDto)
+        {
+            var updatedCategory = ObjectMapper.Mapper.Map<Category>(updateCategoryDto);
+
+            _categoryService.Table.Update(updatedCategory);
+
+            await _categoryService.SaveChangesAsync();
+
+            return Ok(ResponseDto<BlankDto>.Sucess(200));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveMovie([FromRoute] int id)
+        {
+            var movie = await _categoryService.Table.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movie is null)
+                throw new Exception("Category is not found");
+
+            _categoryService.Table.Remove(movie);
+
+            await _categoryService.SaveChangesAsync();
+
+            return Ok(ResponseDto<BlankDto>.Sucess(200));
+        }
+    }
+}
