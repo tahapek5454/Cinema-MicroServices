a68f9e6ffde11ae7659fd85cd930c2575608761a
tahapek5454 <tahapek5454@gmail.com>
2024-02-06 15:38:16 +0300

    feat: add rabbitMQ queue

---
+++ Shared/SharedLibrary/Settings/RabbitMQSettings.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SharedLibrary.Settings
+{
+    public static class RabbitMQSettings
+    {
+        public const string FileStateMachineQueue = "file_state_machine_queue";
+        public const string Movie_MovieImageUploadedQueue = "movie_movie_image_uploaded_event_queue";
+        public const string File_MovieImageRollbackMessageQueue = "file_movie_image_rollback_message_queue";
+
+    }
+}
