335e4a86c6eb5ce3517fb4a4b3d0f5e7df0f1d98
tahapek5454 <tahapek5454@gmail.com>
2024-02-06 15:56:19 +0300

    feat: add MovieImageStateInstance

---
+++ Orchestration/SagaStateMachine.Service/StateInstances/MovieImageStateInstance.cs
@@ -0,0 +1,17 @@
+﻿
+using MassTransit;
+
+namespace SagaStateMachine.Service.StateInstances
+{
+    public class MovieImageStateInstance : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+        public string CurrentState { get; set; }
+
+        public int FileId { get; set; }
+        public string FileName { get; set; }
+        public string Path { get; set; }
+        public string Storage { get; set; }
+        public string RelationId { get; set; }
+    }
+}
