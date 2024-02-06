e35952ab78d55ba2455ca30d68ec46b880c82f1e
tahapek5454 <tahapek5454@gmail.com>
2024-02-06 16:09:17 +0300

    feat: add MovieAppStateDbContext

---
+++ Orchestration/SagaStateMachine.Service/StateDbContexts/MovieAppStateDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using MassTransit.EntityFrameworkCoreIntegration;
+using Microsoft.EntityFrameworkCore;
+using SagaStateMachine.Service.StateMaps;
+
+namespace SagaStateMachine.Service.StateDbContexts
+{
+    public class MovieAppStateDbContext : SagaDbContext
+    {
+        public MovieAppStateDbContext(DbContextOptions options) : base(options) { }
+        protected override IEnumerable<ISagaClassMap> Configurations
+        {
+            get
+            {
+                yield return new MovieImageStateMap();
+            }
+        }
+    }
+}
