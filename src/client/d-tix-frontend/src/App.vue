a372a32601550da9b011b21239a23b65fedd1c0d
gizemkayaa <giizeeem61@gmail.com>
2024-08-20 23:18:02 +0300

    add footer

--- src/client/d-tix-frontend/src/App.vue
+++ src/client/d-tix-frontend/src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavigationDrawer from './components/NavigationDrawer.vue';
-
+import FooterDrawer from './components/FooterDrawer.vue';
 
 </script>
 
 <template>
   <NavigationDrawer/>
   <RouterView />
+  <FooterDrawer/>
 </template>
 
