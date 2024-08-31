66f310adcedc78d2a7cbe60f59414ea869eccde7
tahapek5454 <tahapek5454@gmail.com>
2024-08-31 14:34:33 +0300

    feat: add tailwind

--- src/client/d-tix/src/views/home/index.vue
+++ src/client/d-tix/src/views/home/index.vue
@@ -2,6 +2,9 @@
 <template>
     <div class="home">
       <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+        Oldu mu Be
+      </button>
     </div>
 </template>
   
