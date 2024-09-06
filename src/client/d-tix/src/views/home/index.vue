9c9e18e61f7eaff7dc5bb060eb65c69961ca56ec
tahapek5454 <tahapek5454@gmail.com>
2024-09-06 11:50:07 +0300

    fix: size bug

--- src/client/d-tix/src/views/home/index.vue
+++ src/client/d-tix/src/views/home/index.vue
@@ -6,14 +6,14 @@
         <MovieCarousel></MovieCarousel>
     </div>
 
-    <div class="py-10">
+    <div class="py-10 px-10 md:px-0">
         <div class="container ml-10 my-5">
             <h1 class="text-2xl uppercase">Vizyondakiler</h1>
         </div>
         <MovieCardList/>
     </div>
 
-    <div class="py-10">
+    <div class="py-10 px-10 md:px-0">
         <div class="container ml-10 my-5">
             <h1 class="text-2xl uppercase">Yakında</h1>
         </div>
