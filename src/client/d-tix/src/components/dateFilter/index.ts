462784795d55a774f45ae9354df4ddb3881febb7
gizemkayaa <giizeeem61@gmail.com>
2024-09-15 23:43:56 +0300

    add sessionDateFilter

---
+++ src/client/d-tix/src/components/dateFilter/index.ts
@@ -0,0 +1,134 @@
+import { computed, watch } from 'vue';
+import { Component, Vue, Watch } from 'vue-property-decorator';
+
+@Component
+export default class DateFilter extends Vue {
+  days:string[]=[];
+  visibleItems:string[]=[];
+  daysVisible: number = 4;
+  currentIndex: number = 0;
+
+  created() {
+    this.currentIndex = 0;
+    this.days = this.generateDays(0);
+  }
+
+  private generateDays(index:number): string[] {
+    const today = new Date();
+    today.setDate(today.getDate() + index); // Yarın için başlangıç tarihi
+
+    const days: string[] = [];
+    for (let i = 0; i < this.daysVisible; i++) {
+      const nextDay = new Date(today);
+      nextDay.setDate(today.getDate() + i);
+      const dayString = nextDay.toLocaleDateString('en-GB', { day: '2-digit', month: 'short' });
+      days.push(dayString);
+    }
+    return days;
+  }
+
+  public getDays(): string[] {
+    return this.days;
+  }
+
+  @Watch('currentIndex')
+  changeIndex(newDate: string) {
+   console.log(this.currentIndex);   
+   this.getDays();
+
+  }
+
+  public previous() {
+    if (this.currentIndex > 0) {
+      this.currentIndex --;
+      this.days = this.generateDays(this.currentIndex);
+      console.log(this.days)
+    }
+  }
+
+  public next() {
+    this.currentIndex ++;
+    this.days = this.generateDays(this.currentIndex);
+    console.log(this.days)
+  }
+  
+  sessions: Session[] = [
+    { time: '10:00', date: '15 Sept' },
+    { time: '13:00', date: '15 Sept' },
+    { time: '15:00', date: '15 Sept' },
+    { time: '17:00', date: '15 Sept' },
+    { time: '19:00', date: '15 Sept' },
+    { time: '19:40', date: '15 Sept' },
+    { time: '20:00', date: '15 Sept' },
+    { time: '21:00', date: '15 Sept' },
+    { time: '23:00', date: '15 Sept' },
+    { time: '09:00', date: '16 Sept' },
+    { time: '13:00', date: '16 Sept' },
+    { time: '15:00', date: '16 Sept' },
+    { time: '17:00', date: '16 Sept' },
+    { time: '19:00', date: '16 Sept' },
+    { time: '19:40', date: '16 Sept' },
+    { time: '20:00', date: '16 Sept' },
+    { time: '21:00', date: '16 Sept' },
+    { time: '23:00', date: '16 Sept' },
+    { time: '11:00', date: '17 Sept' },
+    { time: '13:00', date: '17 Sept' },
+    { time: '15:00', date: '17 Sept' },
+    { time: '17:00', date: '17 Sept' },
+    { time: '19:00', date: '17 Sept' },
+    { time: '19:40', date: '17 Sept' },
+    { time: '12:00', date: '18 Sept' },
+    { time: '13:00', date: '18 Sept' },
+    { time: '15:00', date: '18 Sept' },
+    { time: '17:00', date: '18 Sept' },
+    { time: '19:00', date: '18 Sept' },
+    { time: '19:40', date: '18 Sept' },
+    { time: '20:00', date: '18 Sept' },
+    { time: '21:00', date: '18 Sept' },
+  ];
+
+  selectedDate: string = '';
+  selectedSession:string='';
+  filteredResults: Session[] = [];
+
+
+  selectDate(day: string) {
+    this.selectedDate = day;
+  }
+
+  selectSession(session: string) {
+    this.selectedSession = session;
+  }
+
+  @Watch('selectedDate')
+  onSelectedDateChanged(newDate: string) {
+    this.filterResults();
+  }
+
+
+
+  filterResults() {
+    if (this.selectedDate === '') {
+      this.filteredResults = [];
+    } else {
+      this.filteredResults = this.sessions.filter(session => session.date === this.selectedDate);
+    }
+  }
+  
+
+  nextItem (){
+    console.log("next");
+    this.currentIndex ++;
+  }
+
+  previousItem () {
+    console.log("previous");
+    this.currentIndex --;
+  }
+  
+}
+
+export class Session {
+  time: string = '';
+  date: string = '';
+}
---
+++ src/client/d-tix/src/components/dateFilter/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="tw-w-96 tw-mx-auto tw-py-7 tw-px-2 bg-white tw-shadow-md tw-rounded-lg tw-border-2 tw-border-[#8b00008e] tw-mb-5 tw-bg-black">
+        <div class="tw-mt-1 tw-pb-10">
+            <div>
+                <!-- Günlerin Listesi -->
+                <div class="tw-flex tw-space-x-2 tw-mb-1">
+                    <button @click="previous" class="tw-bg-gray-300 tw-rounded-lg  hover:tw-bg-gray-400">
+                          <
+                    </button>
+                    <div
+                        v-for="day in days"
+                        :key="day"
+                        :class="['tw-p-2 tw-w-32 tw-h-8 tw-rounded tw-border-2 tw-border-red tw-flex tw-items-center tw-justify-center tw-grid-cols-4', { 'tw-bg-red tw-text-white': selectedDate === day, 'tw-bg-gray-200': selectedDate !== day }]"
+                        @click="selectDate(day)"
+                    >
+                       <button>
+                         {{ day }}
+                       </button>
+                    </div>
+                    <button @click="next" class="tw-bg-gray-300 tw-rounded-lg  hover:tw-bg-gray-400">
+                        >
+                    </button>
+                </div>
+                
+                <div class="tw-w-full tw-h-96 tw-overflow-hidden tw-items-center tw-mx-4 tw-px-6 tw-mt-9">
+                    <div class="tw-grid tw-gap-4 tw-grid-cols-3 md:tw-grid-cols-3 lg:tw-grid-cols-3">
+                        <div
+                            v-for="session in filteredResults"
+                            :key="session.time"
+                            @click="selectSession(session.time)"
+                            :class="['tw-rounded-lg tw-p-4 tw-w-20 tw-h-2 tw-bg-black tw-text-white tw-shadow-md tw-border-2 tw-border-[#8b00008e] tw-flex tw-items-center tw-justify-center',{'tw-bg-red tw-text-white': selectedSession === session.time, 'tw-bg-gray-200': selectedSession !== session.time }]"
+                            >
+                            <button class="tw-origin-center">
+                                {{ session.time }}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  </template>
+  
+  <script lang="ts"  src="./index"></script>
+
+  
+  <style scoped>
+  /* Burada özel stiller ekleyebilirsiniz */
+  </style>
+  
