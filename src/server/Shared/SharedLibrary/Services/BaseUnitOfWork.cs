5c110c9236cc5a68941ef1090a89b7daae3628a5
tahapek5454 <tahapek5454@gmail.com>
2024-09-15 10:38:29 +0300

    feat: add baseUnitOfWork

---
+++ src/server/Shared/SharedLibrary/Services/BaseUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SharedLibrary.Services
+{
+    public class BaseUnitOfWork
+    {
+        private readonly DbContext _dbContext;
+
+        public BaseUnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int SaveChanges()
+             => _dbContext.SaveChanges();
+
+        public async Task<int> SaveChangesAsync()
+            => await _dbContext.SaveChangesAsync();
+    }
+}
