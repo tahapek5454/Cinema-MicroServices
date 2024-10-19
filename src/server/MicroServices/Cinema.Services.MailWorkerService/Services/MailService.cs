2e211dcd353d994b4cdf18fcc196c77a3c845952
tahapek5454 <tahapek5454@gmail.com>
2024-10-19 16:27:43 +0300

    feat: add mail service

---
+++ src/server/MicroServices/Cinema.Services.MailWorkerService/Services/MailService.cs
@@ -0,0 +1,76 @@
+﻿using System.Net.Mail;
+using System.Net;
+using System.Text;
+using SharedLibrary.Events.MailEvents;
+
+namespace Cinema.Services.MailWorkerService.Services
+{
+    public class MailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public MailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendCompletedReservationMailAsync(ReservationCompleteMailEvent @event)
+        {
+
+            string mail = $$"""
+                    <h3>Merhabalar Sayın {{@event.Name}} {{@event.Surname}}</h3>
+                    <p><b>D-TIX</b>' üzerinden yapmış olduğunuz <b>{{@event.MovieName}}</b> filmi için rezervasyonunuz tamamlanmıştır.</p>
+                    <p>Rezervasyonunuz ile ilgili gerekli bilgiler aşşağıda yer almaktadır.</p>
+
+                    <ul>
+                        <li><b>Rezervasyon Yeri:</b> {{@event.BranchName}} - {{@event.MovieTheaterNumber}} {{@event.CityName}}/{{@event.DistrictName}}</li>
+                        <li><b>Rezervasyon Tarihi:</b> {{@event.ReservationTime.ToString("dd-MM-yyyy HH:MM")}}</li>
+                        <li><b>Koltuk/Koltuklar:</b> {{@event.SeatNumbers}}</li>
+                        <li><b>Toplam Tutar:</b> {{@event.TotalPrice}} TL</li>
+                    </ul>
+
+                    <P>Bizleri tercih ettiğiniz için teşekkür eder ve keyifli seyirler dileriz.</P>
+                    <span><b>D-TIX</b> İletişim: 02735487</span>
+                """;
+
+            await SendMessageAsync(@event.Email, $"Rezervasyonunuz Tamamlandı", mail);
+
+
+        }
+
+        public async Task SendMessageAsync(string[] tos, string subject, string body, bool isBodyHtml = true)
+        {
+            MailMessage mail = new MailMessage();
+            // olusturulan objede html var mı
+            mail.IsBodyHtml = isBodyHtml;
+            // kime gidecegi belirtilmis
+            foreach (var to in tos)
+            {
+                mail.To.Add(to);
+
+            }
+            // subject ve bodylerini yazdik
+            mail.Subject = subject;
+            mail.Body = body;
+
+            // kimin gonderecegini belirtelim
+            mail.From = new MailAddress(_configuration["Mail:UserName"], "D-TIX", System.Text.Encoding.UTF8);
+
+            // GONDERME ISLEMI
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new NetworkCredential(_configuration["Mail:UserName"], _configuration["Mail:ApplicationPassword"]);
+            smtp.Port = Int32.Parse(_configuration["Mail:Port"]);
+            smtp.EnableSsl = true;
+            smtp.Host = _configuration["Mail:Host"];
+            await smtp.SendMailAsync(mail);
+
+        }
+
+        public async Task SendMessageAsync(string to, string subject, string body, bool isBodyHtml = true)
+        {
+            await SendMessageAsync(new string[] { to }, subject, body, isBodyHtml);
+        }
+
+
+    }
+}
---
+++ src/server/Shared/SharedLibrary/Events/MailEvents/ReservationCompleteMailEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace SharedLibrary.Events.MailEvents
+{
+    public class ReservationCompleteMailEvent
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string MovieName { get; set; }
+        public string CityName { get; set; }
+        public string DistrictName { get; set; }
+        public string BranchName { get; set; }
+        public int MovieTheaterNumber { get; set; }
+        public string SeatNumbers { get; set; }
+        public DateTime ReservationTime { get; set; }
+        public string TotalPrice { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
