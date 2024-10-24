3430887227e7c8c1bb6f51c9731bd151e8953fab
tahapek5454 <tahapek5454@gmail.com>
2024-10-24 10:20:36 +0300

    feat: update design

--- src/client/d-tix/src/views/playGround/index.vue
+++ src/client/d-tix/src/views/playGround/index.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="tw-w-full tw-h-[52rem] tw-flex tw-justify-center tw-items-center tw-border tw-border-red">
-
+    <div class="tw-w-full tw-h-[52rem] tw-flex tw-justify-center tw-items-center tw-border tw-border-red tw-space-y-5">
 
       <div class=" tw-rounded-xl tw-w-[40rem] tw-h-[48rem] tw-shadow-xl shadow tw-shadow-white2 tw-bg-black  tw-px-3 tw-flex tw-flex-col tw-text-white2">
           
@@ -11,81 +10,68 @@
 
           <!-- Messajlaşma -->
           <div class="tw-p-4 tw-w-full tw-h-[37rem] tw-rounded-lg tw-bg-background tw-space-y-20 tw-text-xs tw-text-white2 tw-overflow-y-auto" >
-            <div class="tw-flex tw-w-full tw-justify-start">
-              <div class="tw-p-2 tw-w-64 tw-h-auto tw-bg-rose-900 tw-rounded-lg">
-                <span class=" tw-font-sans  tw-w-full tw-text-wrap">Selamlar size nasıl yardımcı olabilirim.</span> 
+
+            <div class="tw-flex">
+              <span class="tw-text-xl material-symbols-outlined tw-cursor-pointer hover:tw-text-rose-900 tw-duration-500">
+                        music_note
+              </span> 
+              <div class="tw-flex tw-w-full tw-justify-start">
+                <div class="tw-p-2 tw-w-64 tw-h-auto tw-bg-rose-900 tw-rounded-lg">
+                  <span class=" tw-font-sans  tw-w-full tw-text-wrap">Selamlar size nasıl yardımcı olabilirim.</span> 
+                </div>
               </div>
             </div>
-              
-            <div class="tw-flex tw-w-full tw-justify-end">
-              <div class="tw-p-2 tw-w-64 tw-h-auto tw-bg-[#3b3a39] tw-rounded-lg">
-                <span class=" tw-font-sans  tw-w-full tw-text-wrap">Vizyondaki en iyi filmi bana söyler misin ?</span> 
+            
+            
+            <div class="tw-flex">
+              <div class="tw-flex tw-w-full tw-justify-end">
+                <div class="tw-p-2 tw-w-64 tw-h-auto tw-bg-[#3b3a39] tw-rounded-lg">
+                  <span class=" tw-font-sans  tw-w-full tw-text-wrap">Vizyondaki en iyi filmi bana söyler misin ?</span> 
+                </div>
               </div>
+              <span class="tw-text-xl material-symbols-outlined tw-cursor-pointer hover:tw-text-rose-900 tw-duration-500">
+                        music_note
+              </span> 
             </div>
 
-            <div class="tw-flex tw-w-full tw-justify-end">
-              <div class="tw-p-2 tw-w-64 tw-h-auto tw-bg-[#3b3a39] tw-rounded-lg">
-                <span class=" tw-font-sans  tw-w-full tw-text-wrap">Vizyondaki en iyi filmi bana söyler misin ?</span> 
-              </div>
-            </div>
-
-
-            <div class="tw-flex tw-w-full tw-justify-end">
-              <div class="tw-p-2 tw-w-64 tw-h-auto tw-bg-[#3b3a39] tw-rounded-lg">
-                <span class=" tw-font-sans  tw-w-full tw-text-wrap">Vizyondaki en iyi filmi bana söyler misin ?</span> 
-              </div>
-            </div>
-            <div class="tw-flex tw-w-full tw-justify-end">
-              <div class="tw-p-2 tw-w-64 tw-h-auto tw-bg-[#3b3a39] tw-rounded-lg">
-                <span class=" tw-font-sans  tw-w-full tw-text-wrap">Vizyondaki en iyi filmi bana söyler misin ?</span> 
-              </div>
-            </div>
-            <div class="tw-flex tw-w-full tw-justify-end">
-              <div class="tw-p-2 tw-w-64 tw-h-auto tw-bg-[#3b3a39] tw-rounded-lg">
-                <span class=" tw-font-sans  tw-w-full tw-text-wrap">Vizyondaki en iyi filmi bana söyler misin ?</span> 
-              </div>
-            </div>
-            <div class="tw-flex tw-w-full tw-justify-end">
-              <div class="tw-p-2 tw-w-64 tw-h-auto tw-bg-[#3b3a39] tw-rounded-lg">
-                <span class=" tw-font-sans  tw-w-full tw-text-wrap">Vizyondaki en iyi filmi bana söyler misin ?</span> 
-              </div>
-            </div>
-            <div class="tw-flex tw-w-full tw-justify-end">
-              <div class="tw-p-2 tw-w-64 tw-h-auto tw-bg-[#3b3a39] tw-rounded-lg">
-                <span class=" tw-font-sans  tw-w-full tw-text-wrap">Vizyondaki asdsadsadsadsa en iyi filmi bana söyler misin ?</span> 
-              </div>
-            </div>
+      
 
           </div>
              <!-- Messajlaşma -->
 
 
-          <div class="tw-flex tw-w-full tw-p-4  tw-justify-around tw-items-center  ">
-            <div class="tw-w-64 tw-p-2 tw-h-9 tw-flex tw-justify-start tw-items-center tw-text-nowrap tw-bg-[#3b3a39] tw-rounded-xl">
-                <span class="tw-text-white2 tw-font-sans tw-text-xs tw-w-full tw-text-wrap">Selamlar nasılsın ?</span> 
-            </div>
-
+          <div class="tw-flex tw-w-full tw-p-4  tw-justify-around   ">
+     
+              <div class="tw-w-[21rem] tw-flex">
+                  <v-text-field class="tw-text-xs tw-font-sans"
+                  filled
+                  rounded
+                  dense
+                  clearable
+                  color="#881337"
+                ></v-text-field>
+              </div>
           
-              <span class="tw-pl-2 tw-text-3xl tw-cursor-pointer material-symbols-outlined hover:tw-text-emerald-400 tw-duration-500">
-                        send
-              </span>
- 
+              <div class="tw-flex tw-space-x-7 tw-pt-1">
 
-          
-              <span class="tw-text-3xl tw-cursor-pointer material-symbols-outlined hover:tw-text-rose-900 tw-duration-500">
-                mic
-              </span>
-       
+                <span class="tw-pl-2 tw-text-3xl tw-cursor-pointer material-symbols-outlined hover:tw-text-emerald-400 tw-duration-500">
+                          send
+                </span>
+  
 
-           
-              <span class="tw-text-3xl tw-cursor-pointer material-symbols-outlined hover:tw-text-blue tw-duration-500">
-                photo_camera
-              </span>
             
-          </div>
+                <span class="tw-text-3xl tw-cursor-pointer material-symbols-outlined hover:tw-text-rose-900 tw-duration-500">
+                  mic
+                </span>
+        
 
+            
+                <span class="tw-text-3xl tw-cursor-pointer material-symbols-outlined hover:tw-text-blue tw-duration-500">
+                  photo_camera
+                </span>
 
-          
+              </div>        
+          </div> 
       </div>
 
  
