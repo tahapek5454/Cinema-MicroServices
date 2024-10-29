7b3f1ba2668917f4257ceadf8524b16eec5b6669
tahapek5454 <tahapek5454@gmail.com>
2024-10-29 19:04:56 +0300

    feat: add user information for conversation

--- src/server/MicroServices/Cinema.Services.AiAssistant/Models/AssistantResponse.cs
+++ src/server/MicroServices/Cinema.Services.AiAssistant/Models/AssistantResponse.cs
@@ -4,5 +4,12 @@
     {
         public string? Message { get; set; }
         public string? ThreadId { get; set; }
+        public Information Information { get; set; }
+    }
+
+    public class Information
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
     }
 }
