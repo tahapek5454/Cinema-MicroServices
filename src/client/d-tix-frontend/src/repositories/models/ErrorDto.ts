6f71bc26e259956e2013ad85f5e31f47afda6f8c
mrtglr <hameguler@gmail.com>
2024-11-11 14:09:59 +0300

    old client files deleted

--- src/client/d-tix-frontend/src/repositories/models/ErrorDto.ts
+++
@@ -1,4 +0,0 @@
-export  interface ErrorDto{
-    Errors? : string[];
-    IsShow: boolean;
-}
--- src/client/d-tix-frontend/src/repositories/models/ResponseDto.ts
+++
@@ -1,8 +0,0 @@
-import type { ErrorDto } from "./ErrorDto";
-
-export interface ResponseDto<T>{
-    data?: T;
-    statusCode?: number;
-    error?: ErrorDto;
-    isSuccessful?: boolean;
-}
