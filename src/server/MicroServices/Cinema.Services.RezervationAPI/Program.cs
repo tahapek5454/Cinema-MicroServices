21bdac8fbffaea67186aad67a67d86a39fed3522
tahapek5454 <tahapek5454@gmail.com>
2024-12-11 15:04:58 +0300

    update

--- src/server/MicroServices/Cinema.Services.RezervationAPI/Program.cs
+++ src/server/MicroServices/Cinema.Services.RezervationAPI/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy",
-        builder => builder.WithOrigins(@"http://localhost:8080", @"https://localhost:8080")
+        builder => builder.AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader()
             );
